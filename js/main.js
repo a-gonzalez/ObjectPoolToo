380194678cb6a90621b029ea1385e6aff8bab1a8
Antonio Gonzalez <agonzalez@gonzaleztek.com>
2024-04-27 23:25:58 -0500

    adding base structure and resources

---
+++ js/main.js
@@ -0,0 +1,33 @@
+import Game from "./game.js";
+
+addEventListener("load", () =>
+{
+    console.log(`Main @ ${new Date().toLocaleString()}`);
+
+    const screen = document.getElementById("screen");
+    screen.width = 600;
+    screen.height = 800;
+
+    const context = screen.getContext("2d");
+    const game = new Game(screen.width, screen.height);
+    //game.test(context);
+
+    let previous_stamp = 0;
+
+    let animate = (time_stamp) =>
+    {
+        const delta_time = time_stamp - previous_stamp;
+        previous_stamp = time_stamp;
+
+        context.clearRect(0, 0, screen.width, screen.height);
+
+        //console.log(delta_time); // 1000 / 60.6 ~ 16.5
+
+        game.draw(context);
+        game.update(delta_time);
+
+        requestAnimationFrame(animate);
+    };
+
+    animate(0);
+});
